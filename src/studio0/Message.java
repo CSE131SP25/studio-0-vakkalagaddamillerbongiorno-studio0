fb28dd6e11b6e971d0f6184d3bddf8172c5dca35
sophiebongiorno <sophiebongiorno@localhost>
2025-01-15 15:33:11 -0600

    testing

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,6 +3,6 @@ package studio0;
 public class Message {
 
 	public static void main(String args[]) {
-		System.out.println(" love the workshop! ");
+		System.out.println(" love love the workshop! ");
 	}   
 }
